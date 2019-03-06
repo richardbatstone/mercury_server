add7126cbcc2cc3385a811033e2844a9dfb5faa0
richardbatstone <36573392+richardbatstone@users.noreply.github.com>
2019-03-06 15:20:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// This is a very small node server which just acts as a wrapper for the Mercury web parser.
+// This is a very small node server which just acts as a wrapper for the Postlight Mercury web parser (https://mercury.postlight.com/)
 // To use it, post to the local host (either with a python request, or with curl e.g.
 //curl -i -H "Content-Type: application/json" -X POST -d "{\"url\":\"https://www.bbc.co.uk/news/world-us-canada-47410846\"}"
 // http://localhost:8080/
